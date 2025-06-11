777967c0af0da4d1a268ade20b8fcc89598a09a0
anirina <dnvslchnk@gmail.com>
2025-06-11 09:52:08 +0300

    add server

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "time"
+)
 
-func main() {
-
+type Response struct {
+    Time string `json:"time"`
 }
 
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+    currentTime := time.Now().Format(time.RFC3339)
+    response := Response{
+        Time: currentTime,
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+    http.HandleFunc("/time", timeHandler)
+
+    fmt.Println("Server started at http://localhost:8795")
+    if err := http.ListenAndServe(":8795", nil); err != nil {
+        fmt.Println("Error starting server:", err)
+    }
+}
